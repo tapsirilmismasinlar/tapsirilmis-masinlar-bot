c37be434922e450a7f21ead460d7dbb5f41b53f6
tapsirilmismasinlar <seymur.blds@mail.ru>
2026-06-13 21:26:47 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,44 +5,42 @@ import os
 
 ADMIN_ID = 956357652
 
-SUPABSUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text("Bot işləyir.")
-async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
-if update.effective_user.id != ADMIN_ID:
-return
+    await update.message.reply_text("Bot işləyir.")
 
-text = " ".join(context.args).upper()
-if not text:
-    await update.message.reply_text("Nömrə yaz.")
-    return
-supabase.table("nomreler").insert({
-    "nomre": text
-}).execute()
-await update.message.reply_text("Əlavə edildi.")
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    text = " ".join(context.args).upper()
+
+    if not text:
+        await update.message.reply_text("Nömrə yaz.")
+        return
+
+    supabase.table("nomreler").insert({"nomre": text}).execute()
+
+    await update.message.reply_text("Əlavə edildi.")
 
 async def check_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
-text = update.message.text.strip().upper()
+    text = update.message.text.strip().upper()
 
-result = (
-    supabase.table("nomreler")
-    .select("nomre")
-    .eq("nomre", text)
-    .execute()
-)
-if result.data:
-    await update.message.reply_text("Nömrə sistemdə mövcuddur")
-else:
-    await update.message.reply_text("Qeydiyyatı yoxdur")
+    result = supabase.table("nomreler").select("nomre").eq("nomre", text).execute()
 
-app = Application.builder().token(“8949021536:AAFXX8r7I0J166Z5fraqpugc-76vFSPyMWM”).build()
+    if result.data:
+        await update.message.reply_text("Nömrə sistemdə mövcuddur")
+    else:
+        await update.message.reply_text("Qeydiyyatı yoxdur")
 
-app.add_handler(CommandHandler(“start”, start))
-app.add_handler(CommandHandler(“add”, add))
+app = Application.builder().token("8949021536:AAFXX8r7I0J166Z5fraqpugc-76vFSPyMWM").build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("add", add))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_number))
 
 app.run_polling()
