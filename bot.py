8c1697895117ffb40d3d6187dcaa2017f342366e
tapsirilmismasinlar <seymur.blds@mail.ru>
2026-06-13 20:57:46 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ import os
 
 ADMIN_ID = 956357652
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv(“SUPABASE_URL”)
+SUPABASE_KEY = os.getenv(“SUPABASE_KEY”)
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -29,16 +29,18 @@ await update.message.reply_text("Əlavə edildi.")
 async def check_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
 text = update.message.text.strip().upper()
 
-result = supabase.table("nomreler")\
-    .select("nomre")\
-    .eq("nomre", text)\
+result = (
+    supabase.table("nomreler")
+    .select("nomre")
+    .eq("nomre", text)
     .execute()
+)
 if result.data:
     await update.message.reply_text("Nömrə sistemdə mövcuddur")
 else:
     await update.message.reply_text("Qeydiyyatı yoxdur")
 
-app = Application.builder().token("8949021536:AAFXX8r7I0J166Z5fraqpugc-76vFSPyMWM").build()
+app = Application.builder().token(“8949021536:AAFXX8r7I0J166Z5fraqpugc-76vFSPyMWM”).build()
 
 app.add_handler(CommandHandler(“start”, start))
 app.add_handler(CommandHandler(“add”, add))
