44bdcc5dc4e5190427f0bd462216f168f664687b
tapsirilmismasinlar <seymur.blds@mail.ru>
2026-06-13 22:19:33 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,44 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+from supabase import create_client
+import os
 
 ADMIN_ID = 956357652
 
-numbers = []
+SUPABASE_URL = os.getenv(“SUPABASE_URL”)
+SUPABASE_KEY = os.getenv(“SUPABASE_KEY”)
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot isleyir.")
+await update.message.reply_text(“Bot isleyir.”)
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user.id != ADMIN_ID:
-        return
+if update.effective_user.id != ADMIN_ID:
+return
 
-    text = " ".join(context.args).upper()
-
-    if not text:
-        await update.message.reply_text("Nomre yaz.")
-        return
-
-    numbers.append(text)
-    await update.message.reply_text("Elave edildi.")
+text = " ".join(context.args).upper()
+if not text:
+    await update.message.reply_text("Nomre yaz.")
+    return
+supabase.table("nomreler").insert({
+    "nomre": text
+}).execute()
+await update.message.reply_text("Elave edildi.")
 
 async def check_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text.strip().upper()
+text = update.message.text.strip().upper()
 
-    if text in numbers:
-        await update.message.reply_text("Movcuddur")
-    else:
-        await update.message.reply_text("Yoxdur")
+result = supabase.table("nomreler").select("nomre").eq("nomre", text).execute()
+if result.data:
+    await update.message.reply_text("Movcuddur")
+else:
+    await update.message.reply_text("Yoxdur")
 
-app = Application.builder().token("8949021536:AAFXX8r7I0J166Z5fraqpugc-76vFSPyMWM").build()
+app = Application.builder().token(“BOT_TOKENINI_BURAYA_YAZ”).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("add", add))
+app.add_handler(CommandHandler(“start”, start))
+app.add_handler(CommandHandler(“add”, add))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_number))
 
 app.run_polling()
